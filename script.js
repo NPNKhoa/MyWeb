a0852d3c8007a221b9922c2096a995cc0cb59fea
NPNKhoa <khoa.nphucnguyen@gmail.com>
2023-09-22 12:24:21 +0700

    Add: timkiem.html, script.js Update: dangnhap.html, trangchu.html, style.css

---
+++ script.js
@@ -0,0 +1,27 @@
+let input = document.getElementById("search");
+let search_button = document.querySelector(".fas.fa-search");
+
+function Submit() {
+  let inputValue = input.value;
+  inputValue = inputValue.trim();
+  if (inputValue != "") {
+    alert(input.value);
+  }
+  input.value = "";
+}
+
+input.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    Submit();
+  }
+});
+
+search_button.addEventListener("click", function (event) {
+  Submit();
+});
+
+function Validate() {
+  let passwd = document.getElementById("password");
+  if (passwd.size < 8) {
+  }
+}
